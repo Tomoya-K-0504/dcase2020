45dbe91a4cffdff3d50b6851e88b14cbb37cf370
Tomoya Koike <makeffort134@gmail.com>
2020-06-04 20:08:44 +0900

    GAN bug fixed

--- tasks/gan_experiment.py
+++ tasks/gan_experiment.py
@@ -6,79 +6,16 @@ from copy import deepcopy
 from datetime import datetime as dt
 from pathlib import Path
 
-import mlflow
 import numpy as np
 import pandas as pd
-import torch
+from experiment import expt_args, label_func, load_func, LoadDataSet
 from joblib import Parallel, delayed
-from librosa.core import load
-from ml.preprocess.preprocessor import Preprocessor
-from ml.src.dataset import ManifestWaveDataSet
-from ml.tasks.base_experiment import BaseExperimentor, typical_train, base_expt_args, get_metric_list
+from ml.tasks.gan_experiment import GANExperimentor
 from ml.utils.notify_slack import notify_slack
 from ml.utils.utils import dump_dict
-from tqdm import tqdm
-
-LABEL2INT = {key: i for i, key in enumerate(
-    ['airport', 'shopping_mall', 'metro_station', 'street_pedestrian', 'public_square', 'street_traffic', 'tram',
-     'bus', 'metro', 'park']
-)}
 
 
-def expt_args(parser):
-    parser = base_expt_args(parser)
-    expt_parser = parser.add_argument_group("DCASE2020 Experiment arguments")
-    expt_parser.add_argument('--n-parallel', default=1, type=int)
-    expt_parser.add_argument('--only-test', action='store_true')
-    expt_parser.add_argument('--mlflow', action='store_true')
-    expt_parser.add_argument('--notify-slack', action='store_true')
-
-    return parser
-
-
-def label_func(row):
-    return LABEL2INT[row[1]]
-
-
-def load_func(path):
-    wave = load(f'{path[0]}', sr=44100)[0]
-    assert wave.shape[0] / 44100 == 10
-    return wave[None, :]
-
-
-class LoadDataSet(ManifestWaveDataSet):
-    def __init__(self, manifest_path, data_conf, phase='train', load_func=None, transform=None, label_func=None):
-        super(LoadDataSet, self).__init__(manifest_path, data_conf, phase, load_func, transform, label_func)
-
-    def __getitem__(self, idx):
-        try:
-            path = Path(self.path_df.iloc[idx, 0])
-            x = torch.load(str(path.parents[2] / 'processed' / path.name.replace('.wav', '.pt')))
-        except FileNotFoundError as e:
-            print(e)
-            return super().__getitem__(idx)
-
-        label = self.labels[idx]
-
-        return x, label
-
-
-def parallel_logmel(expt_conf, load_func, label_func, phases):
-    def parallel_preprocess(dataset, idx):
-        processed, _ = dataset[idx]
-        path = Path(dataset.path_df.iloc[idx, 0])
-        torch.save(processed.to('cpu'), str(path.parents[2] / 'processed' / path.name.replace('.wav', '.pt')))
-
-    for phase in tqdm(phases):
-        process_func = Preprocessor(expt_conf, phase).preprocess
-        dataset = ManifestWaveDataSet(expt_conf[f'{phase}_path'], expt_conf, phase, load_func, process_func,
-                                      label_func)
-        Parallel(n_jobs=8, verbose=0)(
-            [delayed(parallel_preprocess)(dataset, idx) for idx in range(len(dataset))])
-        print(f'{phase} done')
-
-
-def main(expt_conf, hyperparameters, typical_train_func):
+def main(expt_conf, hyperparameters):
     if expt_conf['expt_id'] == 'timestamp':
         expt_conf['expt_id'] = dt.today().strftime('%Y-%m-%d_%H:%M')
 
@@ -121,14 +58,13 @@ def main(expt_conf, hyperparameters, typical_train_func):
         expt_conf['model_path'] = str(expt_dir / f"{'_'.join([str(p).replace('/', '-') for p in pattern])}.pth")
         expt_conf['log_id'] = f"{'_'.join([str(p).replace('/', '-') for p in pattern])}"
 
-        with mlflow.start_run():
-            result_series, val_pred, _ = typical_train_func(expt_conf, load_func, label_func, process_func=None,
-                                                            dataset_cls=dataset_cls, groups=groups)
+        # with mlflow.start_run():
+        GANExperimentor(expt_conf, load_func, label_func, process_func=None, dataset_cls=dataset_cls)._experiment(metrics_names, phases=['train', 'val'])
 
-            mlflow.log_params({hyperparameter: value for hyperparameter, value in zip(hyperparameters.keys(), pattern)})
-            mlflow.log_artifacts(expt_dir)
+            # mlflow.log_params({hyperparameter: value for hyperparameter, value in zip(hyperparameters.keys(), pattern)})
+            # mlflow.log_artifacts(expt_dir)
 
-        return result_series, val_pred
+        return
 
     if expt_conf['only_test']:
         val_results = pd.read_csv(expt_dir / 'val_results.csv')
@@ -161,38 +97,6 @@ def main(expt_conf, hyperparameters, typical_train_func):
         expt_conf[param] = best_pattern[i]
     dump_dict(expt_dir / 'best_parameters.txt', {p: v for p, v in zip(hyperparameters.keys(), best_pattern)})
 
-    # Train with train + devel dataset
-    phases = ['train', 'infer']
-    if expt_conf['only_test'] or expt_conf['infer']:
-        expt_conf['model_path'] = str(expt_dir / f"{'_'.join([str(p).replace('/', '-') for p in best_pattern])}_all.pth")
-
-        # parallel_logmel(expt_conf, load_func, label_func, ['train', 'infer'])
-
-        dataset_cls = LoadDataSet
-        # dataset_cls = ManifestWaveDataSet
-
-        experimentor = BaseExperimentor(expt_conf, load_func, label_func, process_func=None, dataset_cls=dataset_cls)
-
-        metrics = {p: get_metric_list(metrics_names[p]) for p in phases}
-        # _, pred = experimentor.experiment_without_validation(metrics, seed_average=expt_conf['n_seed_average'])
-        _, pred = experimentor._experiment(metrics, phases=['infer'])
-        expt_conf['model_path'] = str(expt_dir / f"{'_'.join([str(p).replace('/', '-') for p in best_pattern])}_all.pth")
-        experimentor.train_manager.model_manager.save_model()
-
-        sub_name = f"sub_{'_'.join([str(p).replace('/', '-') for p in best_pattern])}.csv"
-        # if (expt_dir / sub_name).is_file():
-        #     sub_df = pd.read_csv(expt_dir / sub_name)
-        # else:
-        #     sub_df = manifest_df[manifest_df['file_name'].str.startswith('test')][['file_name']].reset_index(drop=True)
-
-        # if expt_conf['return_prob']:
-        #     pd.DataFrame(pred['infer']).to_csv(expt_dir / f'prob_{sub_name}', index=False)
-        #     pred['infer'] = np.argmax(pred['infer'], axis=1)
-        #
-        # sub_df['prediction'] = pd.Series(pred['infer']).apply(lambda x: list(LABEL2INT.keys())[x])
-        # sub_df.to_csv(expt_dir / sub_name, index=False)
-        # print(f"Submission file is saved in {expt_dir / sub_name}")
-
 
 def create_manifest(expt_conf):
     data_path = Path(expt_conf[f'train_path']).parents[1]
@@ -223,7 +127,7 @@ if __name__ == '__main__':
             'model_type': ['panns_wavegram'],
             'transform': ['logmel'],
             # 'checkpoint_path': ['../cnn14.pth'],
-            'window_size': [0.15],
+            'window_size': [0.06],
             'window_stride': [0.05],
             'epoch_rate': [0.05],
             'mixup_alpha': [0.0],
@@ -241,7 +145,7 @@ if __name__ == '__main__':
             'kl_penalty': [0.0],
             'entropy_penalty': [0.0],
             'loss_func': ['ce'],
-            'window_size': [0.15],
+            'window_size': [0.1],
             'window_stride': [0.05],
             'epoch_rate': [1.0],
             'mixup_alpha': [0.0],
@@ -252,7 +156,7 @@ if __name__ == '__main__':
 
     expt_conf = create_manifest(expt_conf)
 
-    main(expt_conf, hyperparameters, typical_train)
+    main(expt_conf, hyperparameters)
 
     if not expt_conf['mlflow']:
         import shutil
